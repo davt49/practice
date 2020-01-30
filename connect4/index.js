14fad1641794a18aa24b19b7d926d334e4f78c28
davidtsui <davidtsuiart@gmail.com>
2020-01-30 22:45:35 +0000

    added css

--- connect4/index.js
+++ connect4/index.js
@@ -1,19 +1,22 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
   const grid = document.querySelector('.grid')
+  const board = []
   const height = 6
   const width = 7
+  
 
   function makeGrid(){
     for (i = 0; i < height * width; i++){
       const square = document.createElement('div')
       square.classList.add('grid-item')
       square.setAttribute('id', `${i}`)
-      square.style.backgroundColor = 'rgba(217, 217, 217, 0.5)'
-      square.style.height = '14vh'
-      square.style.width = '13vw'
-      square.style.margin = '1px 2px 1px 2px'
-      square.style.display = 'inline-block'
+      // square.style.backgroundColor = 'rgba(217, 217, 217, 0.5)'
+      // square.style.height = '14vh'
+      // square.style.width = '13vw'
+      // square.style.margin = '1px 2px 1px 2px'
+      // square.style.display = 'inline-block'
+      board.push(square)
       grid.append(square)
     }
   }
@@ -22,17 +25,42 @@ document.addEventListener('DOMContentLoaded', () =>{
 
   grid.style.margin = '50px 10px 10px 30px'
   
-  // const playerOneMO
-  // const playerTwoMO
+  
+  document.querySelector('.player-one-title').style.color = 'black'
+  document.querySelector('.player-two-title').style.color = 'yellow'
+  
+  let playerOne = 0
+  let playerOneHover = board[playerOne]
+  playerOneHover.classList.add('active')
+  
+  let playerTwo = 6
+  let playerTwoHover = board[playerTwo]
+  playerTwoHover.classList.add('active2')
+
+  console.log(board[0])
+  console.log(playerOne)
+  console.log(board[playerOne + 1])
+  test = board[playerOne + 1]
+  test.classList.add('active')
+
+  function handleKeyDown(e){
+
+      if (e.keyCode === 97 && playerOne > 0){
+        
+        playerOne--
+        console.log(e.keyCode + ' ' + playerOne)
+        
+      }
+
+      else if (e.keyCode === 115 && playerOne < 6){
+
+        playerOne++
+        console.log(e.keyCode + ' ' + playerOne)
+        
+      } return 
+    }
 
 
-  // function playerOne(e){
-  //   switch(e.keyCode){
-  //     case 65://A
-
-  //     case 53://S
-
-  //   }
-  // }
+    window.addEventListener('keypress', handleKeyDown)
 
 })
