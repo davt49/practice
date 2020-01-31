a95311c516467394aafec69a71ddc7c059a3aee2
davidtsui <davidtsuiart@gmail.com>
2020-01-31 15:04:14 +0000

    box move logic

--- connect4/index.js
+++ connect4/index.js
@@ -11,11 +11,6 @@ document.addEventListener('DOMContentLoaded', () =>{
       const square = document.createElement('div')
       square.classList.add('grid-item')
       square.setAttribute('id', `${i}`)
-      // square.style.backgroundColor = 'rgba(217, 217, 217, 0.5)'
-      // square.style.height = '14vh'
-      // square.style.width = '13vw'
-      // square.style.margin = '1px 2px 1px 2px'
-      // square.style.display = 'inline-block'
       board.push(square)
       grid.append(square)
     }
@@ -24,40 +19,61 @@ document.addEventListener('DOMContentLoaded', () =>{
   makeGrid()
 
   grid.style.margin = '50px 10px 10px 30px'
-  
-  
   document.querySelector('.player-one-title').style.color = 'black'
   document.querySelector('.player-two-title').style.color = 'yellow'
   
-  let playerOne = 0
-  let playerOneHover = board[playerOne]
-  playerOneHover.classList.add('active')
+  let playerOne
+  let playerTwo
+
+
+  function startbox(){
+    playerOne = 0
+    playerTwo = 6
+
+    board[playerOne].classList.add('active')
+    // board[playerTwo].classList.add('active2')
+  }
+  startbox()
+
+
   
-  let playerTwo = 6
-  let playerTwoHover = board[playerTwo]
-  playerTwoHover.classList.add('active2')
-
-  console.log(board[0])
-  console.log(playerOne)
-  console.log(board[playerOne + 1])
-  test = board[playerOne + 1]
-  test.classList.add('active')
-
+  
   function handleKeyDown(e){
+ 
+    switch(e.keyCode){
+      case 97:
+        if (playerOne !== 0){  
+          if (playerOne < 0 || playerOne === -1) {
+            playerOne = 6
+          }
+          if (playerOne > 6 || playerOne === 7) {
+            playerOne = 0
+          }
+          board.forEach(square => square.classList.remove('active'))
+         
+          playerOne = playerOne - 1
+          board[playerOne].classList.add('active')
 
-      if (e.keyCode === 97 && playerOne > 0){
-        
-        playerOne--
-        console.log(e.keyCode + ' ' + playerOne)
-        
+        }
+        break
+      case 115: 
+        if(playerOne !== 6){
+          if (playerOne < 0 || playerOne === -1) {
+            playerOne = 6
+          }
+          if (playerOne > 6 || playerOne === 7) {
+            playerOne = 0
+          }
+          board.forEach(square => square.classList.remove('active'))
+          
+          playerOne = playerOne + 1
+          board[playerOne].classList.add('active')
+  
+        }  
+        break
+        default:
       }
-
-      else if (e.keyCode === 115 && playerOne < 6){
-
-        playerOne++
-        console.log(e.keyCode + ' ' + playerOne)
-        
-      } return 
+    console.log(playerOne)
     }
 
 
