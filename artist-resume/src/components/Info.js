6189436477434c6a8ea88344faa016fa9e21ba1f
davidtsui <davidtsuiart@gmail.com>
2020-02-12 14:57:36 +0000

    to add node

--- artist-resume/src/components/Info.js
+++ artist-resume/src/components/Info.js
@@ -6,26 +6,46 @@ class Info extends React.Component{
     super()
 
     this.state = {
-      data:{}
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    // this.handleSubmit = this.handleSubmit.bind(this)
+    // this.checkValue = this.checkValue.bind(this)
   }
 
-  handleChange(){}
+  handleChange({target: {name, value}}){
+    const data = {...this.state.value, [name]: value}
+    this.setState({data})
+    console.log(this.state)
+  }
+
+  // handleSubmit(event){
+  //   this.setState({saved: this.state})
+  //   event.preventDefault()
+  // }
+  
+  // checkValue(){
+  //   console.log(this.state)
+  // }
+  // componentDidMount(){
+  //   setInterval(this.checkValue, 4000)
+  // }
 
   render(){
     return(
       <div>
         <form className='form'>
-          <input type='text' placeholder='Name'></input>
-          <input type='text' placeholder='Stage Name'></input>
-          <input type='text' placeholder='Address'></input>
-          <input type='text' placeholder='Email'></input>
-          <input type='text' placeholder='Telephone Number'></input>
-          <input type='text' placeholder='Skills'></input>
-          <input type='text' placeholder='Languages'></input>
+          <input type='text' placeholder='Name' name='Name' onChange={this.handleChange}></input>
+          <input type='text' placeholder='Stage Name' name='Stage Name' onChange={this.handleChange}></input>
+          <input type='text' placeholder='Address' name='Address' onChange={this.handleChange}></input>
+          <input type='text' placeholder='Email' name='Email' onChange={this.handleChange}></input>
+          <input type='text' placeholder='Telephone Number' name='Telephone' onChange={this.handleChange}></input>
+          <input type='text' placeholder='Skills' name='Skills' onChange={this.handleChange}></input>
+          <input type='text' placeholder='Languages' name='Languages' onChange={this.handleChange}></input>
           <div className='text-area'>
             <textarea placeholder='Biography' className='big-input'></textarea>
           </div>
+          <button>Submit</button>
         </form>
       </div>
     )
